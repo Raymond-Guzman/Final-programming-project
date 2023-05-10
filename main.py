67250a5155b23bc75ed49f4debb0f6cd258cdb59
[200~        name = Raymond-Guzman <noahgamingboss2231@gmail.com>
2023-05-10 09:22:13 -0600

    start of player class

---
+++ main.py
@@ -0,0 +1,42 @@
+
+import pickle
+
+class player():
+
+	def __init__(self):
+		self.inventory = []
+		self.name = input("What is your name?")
+		print(f"{self.name} has entered the forest.")
+
+	def choices(question = "What would you like to do?"):
+		
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main(player):
